c08dfecde9e3327ec67fd24dad4172074bae72e4
Елена Николаева <elena@192.168.0.10>
2022-06-26 18:58:36 +0300

    Новая ветка Новая папка ДЗ

---
+++ 1_Lesson_HW/HW_1/Program.cs
@@ -0,0 +1,9 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+
+Console.Write("max = ");
+Console.WriteLine(max);

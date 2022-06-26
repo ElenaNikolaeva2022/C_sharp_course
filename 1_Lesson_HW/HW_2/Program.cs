91bc73f38de050c82ca4320b601f68973e5f3c6b
Елена Николаева <elena@192.168.0.10>
2022-06-26 20:30:18 +0300

    HW 2

---
+++ 1_Lesson_HW/HW_2/Program.cs
@@ -0,0 +1,9 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.Write("max = ");
+Console.WriteLine(max);

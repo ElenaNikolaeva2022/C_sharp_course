c331110c58f7c4207732fce429158949d5829071
Елена Николаева <elena@192.168.0.10>
2022-06-29 18:04:53 +0300

    HW 2 1

---
+++ 2_Lesson/HW_2_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого
+// числа. (456  - 5), (782 - 8), (918 - 1)
+
+
+void OneOfNum(int num_1, int num_2)
+{
+    if (num_1 % num_2 == 0)
+    {
+        // multiple (множественный)
+        Console.WriteLine($"{num_1}, {num_2} -> multiple");
+    }
+    else
+    {
+        // not multiple remainder (не множественный остаток)
+        Console.WriteLine($"{num_1}, {num_2} -> not multiple, the remainder {num_1 % num_2}");
+    }
+}
+OneOfNum(34, 5);
+OneOfNum(16, 4);

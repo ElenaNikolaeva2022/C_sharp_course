86e7d0928aa30a2046f4419d7f047baf3815cb67
Елена Николаева <elena@192.168.0.10>
2022-07-04 17:10:02 +0300

    в ДЗ добавлены дополнительные решения задач

--- 3_Lesson/3_Lesson_HW/HW_3_3/Program.cs
+++ 3_Lesson/3_Lesson_HW/HW_3_3/Program.cs
@@ -17,3 +17,25 @@ void Cube(int num)
 
 Cube(3);
 Cube(5);
+
+
+// void Squares(int n)
+// {
+//     double[] nums = new double[n];
+//     int index = 0;
+
+//     Console.WriteLine($"{n} -> ");
+//     while (index < n)
+//     {
+//         nums[index] = Math.Pow((index + 1), 3);
+//         Console.WriteLine($"{nums[index]}, ");
+//         index += 1;
+//     }
+//     Console.WriteLine();
+// }
+
+// Squares(6);
+// Squares(2);
+// Squares(11);
+
+

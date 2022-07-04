a56fb890ae7a5e14be7b7852f5b91609bc6a2806
Елена Николаева <elena@192.168.0.10>
2022-07-04 11:15:00 +0300

    1_Lesson Добавила условие задач

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,4 +1,12 @@
-﻿// string num_str = Console.ReadLine();
+﻿//1. Напишите программу, которая на вход принимает два числа
+// и проверяет, является ли первое число квадратом второго. 
+
+// a = 5; b = 25 -> да
+// a = 2 b = 10 -> нет 
+// a = 9; b = -3 -> нет 
+// a = -3 b = 9 -> да
+
+// string num_str = Console.ReadLine();
 // int num = int.Parse(num_str);
 
 int num1 = int.Parse(Console.ReadLine());

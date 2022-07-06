670caefb93c980d9f8d8da8d12b29d9af7272a04
Елена Николаева <elena@192.168.0.10>
2022-07-06 23:39:01 +0300

    Добавлены доп решения

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,4 +1,28 @@
-﻿// 1. Напишите программу, которая на вход принимает 
+﻿// Варианты записи
+// Развёрнутый вариант:
+// string num_str = Console.ReadLine();
+// int num = int.Parse(num_str);
+
+// Компактный вариант. И в терминале пишем любую цифру:
+// например прибаим к числу 10. СЛОЖЕНИЕ.
+// int num = int.Parse(Console.ReadLine());
+// Console.WriteLine(num + 10);
+
+// 0. Демонстрация решения. 
+// Напишите программу, которая на вход принимает число 
+// и выдаёт его квадрат (число умноженное на само себя). 
+// Например: 
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+// int num = int.Parse(Console.ReadLine());
+// Console.WriteLine(num * num);
+
+// Ещё можно сделать красиво:
+// int num = int.Parse(Console.ReadLine());
+// Console.WriteLine($"{num}^2: {num * num}");
+
+// 1. Напишите программу, которая на вход принимает 
 // два числа и проверяет, является ли первое число квадратом второго. 
 
 // a = 5; b = 25 -> да 
@@ -6,9 +30,19 @@
 // a = 9; b = -3 -> нет
 // a = -3 b = 9 -> да 
 
-
-// string num_str = Console.ReadLine();
-// int num = int.Parse(num_str);
+// Решение Марии:
+// string s_a = Console.ReadLine();
+// string s_b = Console.ReadLine();
+// int a = int.Parse(s_a);
+// int b = int.Parse(s_b);
+// if (b == a * a)
+// {
+// Console.WriteLine("Yes");
+// }
+// else
+// {
+//   Console.WriteLine("No");  
+// }
 
 
 int num1 = int.Parse(Console.ReadLine());

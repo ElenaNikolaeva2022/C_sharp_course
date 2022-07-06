2e95908dea5393759caedc3300802e47ff6a2ac9
Елена Николаева <elena@192.168.0.10>
2022-07-07 00:37:35 +0300

    Дбавлено доп решение

--- 1_Lesson_HW/HW_1/Program.cs
+++ 1_Lesson_HW/HW_1/Program.cs
@@ -13,3 +13,25 @@ if (num2 > max) max = num2;
 
 Console.Write("max = ");
 Console.WriteLine(max);
+
+// Решение Марии:
+// string s_n_1 = Console.ReadLine();
+// string s_n_2 = Console.ReadLine();
+
+// int n_1 = int.Parse(s_n_1);
+// int n_2 = int.Parse(s_n_2);
+
+// if (n_1 > n_2)
+// {
+//     Console.WriteLine($"The number {n_1} is greater than {n_2}");
+// }
+// else if (n_1 == n_2)
+// {
+//     Console.WriteLine($"The number {n_1} is equal to the number {n_2}");
+// }
+// else
+// {
+//     Console.WriteLine($"The number {n_2} is greater than {n_1}");
+// }
+
+

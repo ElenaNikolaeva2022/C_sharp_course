484f90a0a931dfc292a8cf95cd7fab6157d4be89
Елена Николаева <elena@192.168.0.10>
2022-07-06 13:25:41 +0300

    Добавлено вручную в main из ветки 3_Lesson

---
+++ 3_Lesson/3_Lesson_HW/HW_3_1/Program.cs
@@ -0,0 +1,60 @@
+﻿// Палиндром — это такие слова, числа или символы,
+// которые читаются одинаково в обоих направлениях.
+// Палиндромы бывают образованы одним словом,
+// состоящим из 1-2 символов, а могут быть 
+// образованы целыми текстами.
+
+// 1.Напишите программу, которая принимает на вход пятизначное
+// число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+// void Palindrome(int n)
+// {
+//     int i = 0;
+//     string num = n.ToString();
+//     int size = num.Length - 1;
+//     string flag = "Нет";
+
+//     while (i < size / 2)
+//     {
+//         if (num[i] == num[size - i])
+//         {
+//             flag = "Да";
+//             break;
+//         }
+//         i += 1;
+//     }
+//     Console.WriteLine($"{n} -> {flag}");
+// }
+
+// Palindrome(14212);
+// Palindrome(12821);
+// Palindrome(23432);
+
+void Palindrome(int n)
+{
+    int i = 0;
+    string num = n.ToString();
+    int size = num.Length - 1;
+    string flag = "Да";
+
+    while (i < size / 2)
+    {
+        if (num[i] != num[size - i])
+        {
+            flag = "Нет";
+            break;
+        }
+        i += 1;
+    }
+    Console.WriteLine($"{n} -> {flag}");
+}
+
+Palindrome(14212);
+Palindrome(12821);
+Palindrome(23432);
+Palindrome(1248421);
+Palindrome(12488421);
---
+++ 3_Lesson/3_Lesson_HW/HW_3_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// https://ru.onlinemschool.com/math/library/analytic_geometry/point_point_length/
+// Формулы вычисления расстояния между двумя точками:
+// A(xa, ya, za) и B(xb, yb, zb) в пространстве:
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+
+// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double AB(double ax, double ay, double az, double bx, double by, double bz)
+{
+    return Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+}
+
+Console.WriteLine(AB(3, 6, 8, 2, 1, -7));
+
+// double Line3D(float x1, float y1, float z1, float x2, float y2, float z2)
+// {
+//   return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);  
+// }
+// Console.WriteLine(Line3D(3, 6, 8, 2, 1, -7));
+// Console.WriteLine(Line3D(7, -5, 0, 1, -1, 9));
+
+

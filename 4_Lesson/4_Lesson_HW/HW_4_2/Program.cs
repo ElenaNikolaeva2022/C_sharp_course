f33137327c3868b94a113593044120324599b42f
Елена Николаева <elena@192.168.0.10>
2022-07-07 15:57:17 +0300

    4_2 4_3 дополнительные решения

--- 4_Lesson/4_Lesson_HW/HW_4_2/Program.cs
+++ 4_Lesson/4_Lesson_HW/HW_4_2/Program.cs
@@ -4,14 +4,45 @@
 // 82 -> 10
 // 9012 -> 12
 
-int Num = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
+// Решение:
+// int Num = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
 
-while (Num > 0)
+// while (Num > 0)
+// {
+//     int num = Num % 10;
+//     Num = Num / 10;
+//     sum = sum + num;
+// }
+
+// Console.WriteLine(sum);
+
+// Вариант1. Решение:
+// int SumNums(int num)
+// {
+//     int n_sum = 0;
+//     while (num > 0)
+//     {
+//         n_sum += num % 10;
+//         num /= 10;
+//     }
+//     return n_sum;
+// }
+// Console.WriteLine(SumNums(452));
+// Console.WriteLine(SumNums(82));
+// Console.WriteLine(SumNums(9012));
+
+
+// Вариант2. Решение:
+int SumNums(int num)
 {
-    int num = Num % 10;
-    Num = Num / 10;
-    sum = sum + num;
-}
+    int n_sum = 0;
 
-Console.WriteLine(sum);
+    for (int i = num; num != 0; num /= 10)
+        n_sum += num % 10;
+
+    return n_sum;
+}
+Console.WriteLine(SumNums(452));
+Console.WriteLine(SumNums(82));
+Console.WriteLine(SumNums(9012));

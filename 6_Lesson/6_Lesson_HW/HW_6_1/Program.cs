675cb93b7bbf59375a9f8c3d2185db46b720895a
Елена Николаева <elena@192.168.0.10>
2022-07-14 15:48:28 +0300

    HW_6_1 Коррект

--- 6_Lesson/6_Lesson_HW/HW_6_1/Program.cs
+++ 6_Lesson/6_Lesson_HW/HW_6_1/Program.cs
@@ -1,40 +1,40 @@
 ﻿// 1: Пользователь вводит с клавиатуры M чисел. Посчитайте,
-//    сколько чисел больше 0 ввёл пользователь.
+//    сколько чисел > 0 ввёл пользователь.
 //    0, 7, 8, -2, -2 -> 2
 //    -1, -7, 567, 89, 223-> 3
 
 // 1. ВАРИАНТ:
-// int CountNum()
-// {
-//     int count = 0;
-//     string word;
-
-//     while (true)
-//     {
-//         Console.Write("Выведите число: ");
-//         word = Console.ReadLine();
-
-//         if (word == "") return count;
-//         else if (int.Parse(word) > 0) count += 1;
-//     }
-// }
-
-//  Console.Write(CountNum());
-
-// 2. ВАРИАНТ:
-int Check(int num)
+int NumM()
 {
     int count = 0;
+    string word;
 
-    for (int i = 1; i <= num; i++)
+    while (true)
     {
-        Console.Write($"Число {i}: ");
-        int a = int.Parse(Console.ReadLine());
-        if (a > 0) count++;
+        Console.Write("Output any number: ");
+        word = Console.ReadLine();
+
+        if (word == "") return count;
+        else if (int.Parse(word) > 0) count += 1;
     }
-    return count;
 }
 
- Console.Write("Введите число: ");
- int a = int.Parse(Console.ReadLine());
- Console.WriteLine($"Количество чисел > 0: {Check(a)}");
+Console.Write(NumM());
+
+// 2. ВАРИАНТ:
+// int Check(int num)
+// {
+//     int count = 0;
+
+//     for (int i = 1; i <= num; i++)
+//     {
+//         Console.Write($"Число {i}: ");
+//         int a = int.Parse(Console.ReadLine());
+//         if (a > 0) count++;
+//     }
+//     return count;
+// }
+
+// Console.Write("Сколько чисел хотите ввести?: ");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Количество чисел > 0: {Check(a)}");

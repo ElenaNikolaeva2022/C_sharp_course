d16b7fd94ec0fa69cc1134360b9a33a94a71b2e1
Елена Николаева <elena@192.168.0.10>
2022-07-29 23:20:37 +0300

    7_1_Lesson Дополнение

--- 7_1_Lesson/7_1_Lesson_HW/HW_7_1_2/Program.cs
+++ 7_1_Lesson/7_1_Lesson_HW/HW_7_1_2/Program.cs
@@ -7,51 +7,95 @@
 //    8 4 2 4
 //    1 7 -> такого числа в массиве нет
 
-int[,] DoubleArray(int row, int column, int from, int to)
+// int[,] DoubleArray(int row, int column, int from, int to)
+// {
+//     int[,] array = new int[row, column];
+
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j < row; j++)
+//         {
+//             array[i, j] = new Random().Next(from, to);
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintDoubleArray(int[,] array)
+// {
+//     int rows = array.GetLength(0);
+//     int columns = array.GetLength(1);
+
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < rows; j++)
+//         {
+//             Console.Write($" {array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int[,] array = DoubleArray(4, 4, 0, 10);
+// PrintDoubleArray(array);
+
+// Console.Write("Введите номер строки: ");
+// int row = int.Parse(Console.ReadLine());
+// Console.Write("Введите номер столбца: ");
+// int column = int.Parse(Console.ReadLine());
+
+// if (row < 1 || column < 1 || row > array.GetLength(0) || column > array.GetLength(1))
+// {
+//     Console.WriteLine("такого элемента нет ");
+// }
+
+// else
+// {
+//     Console.WriteLine($"На позиции {row}; {column} находится {array[row - 1, column - 1]} ");
+// }
+
+
+void Print(int[,] arr)
 {
-    int[,] array = new int[row, column];
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
 
-    for (int i = 0; i < row; i++)
+    for (int i = 0; i < row_size; i++)
     {
-        for (int j = 0; j < row; j++)
-        {
-            array[i, j] = new Random().Next(from, to);
-        }
-    }
-    return array;
-}
-
-void PrintDoubleArray(int[,] array)
-{
-    int rows = array.GetLength(0);
-    int columns = array.GetLength(1);
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < rows; j++)
-        {
-            Console.Write($" {array[i, j]} ");
-        }
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-int[,] array = DoubleArray(4, 4, 0, 10);
-PrintDoubleArray(array);
-
-Console.Write("Введите номер строки: ");
-int row = int.Parse(Console.ReadLine());
-Console.Write("Введите номер столбца: ");
-int column = int.Parse(Console.ReadLine());
-
-if (row < 1 || column < 1 || row > array.GetLength(0) || column > array.GetLength(1))
+int[,] MassNums(int row, int column, int from, int to)
 {
-    Console.WriteLine("такого элемента нет ");
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
 }
 
-else
+string FindElement(int[,] arr, int f, int s)
 {
-    Console.WriteLine($"На позиции {row}; {column} находится {array[row - 1, column - 1]} ");
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    if (f > row || f <= 0 || s > column || s <= 0)
+        return $"{f} {s} -> not in the array";
+    return $"arr[{f}, {s}] = {arr[f - 1, s - 1]} -> is in the array";
 }
 
+Console.Write("Enter the line position: ");
+int first = int.Parse(Console.ReadLine());
+Console.Write("Enter the column position: ");
+int second = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(3, 4, 1, 11);   // 1 4 7 2 
+Print(arr_1);
+
+Console.WriteLine(FindElement(arr_1, first, second));
--- 7_1_Lesson/7_1_Lesson_HW/HW_7_1_3/Program.cs
+++ 7_1_Lesson/7_1_Lesson_HW/HW_7_1_3/Program.cs
@@ -7,65 +7,115 @@
 //    Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
-int[,] DoubleArray(int row, int column, int from, int to)
+
+// int[,] DoubleArray(int row, int column, int from, int to)
+// {
+//     int[,] array = new int[row, column];
+
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j < column; j++)
+//         {
+//             array[i, j] = new Random().Next(from, to);
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintDoubleArray(int[,] array)
+// {
+//     int rows = array.GetLength(0);
+//     int columns = array.GetLength(1);
+
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             Console.Write($" {array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// double[] MeanArithmetic(int[,] array)
+// {
+//     int rows = array.GetLength(0);
+//     int columns = array.GetLength(1);
+//     double[] avg = new double[columns];
+
+//     for (int j = 0; j < columns; j++)
+//     {
+//         int sum = 0;
+//         for (int i = 0; i < rows; i++)
+//         {
+//             sum += array[i, j];
+//         }
+//         avg[j] = Math.Round((double)sum / rows, 1);
+//     }
+//     return avg;
+// }
+
+// void PrintArray(double[] array)
+// {
+//     int length = array.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         Console.Write(array[i]);
+//         if (i < length - 1) Console.Write("; ");
+//         else Console.WriteLine();
+//     }
+// }
+
+// int[,] array = DoubleArray(3, 4, 1, 10);
+// PrintDoubleArray(array);
+// PrintArray(MeanArithmetic(array));
+
+
+void Print(int[,] arr)
 {
-    int[,] array = new int[row, column];
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
 
-    for (int i = 0; i < row; i++)
+    for (int i = 0; i < row_size; i++)
     {
-        for (int j = 0; j < column; j++)
-        {
-            array[i, j] = new Random().Next(from, to);
-        }
-    }
-    return array;
-}
-
-void PrintDoubleArray(int[,] array)
-{
-    int rows = array.GetLength(0);
-    int columns = array.GetLength(1);
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            Console.Write($" {array[i, j]} ");
-        }
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-double[] MeanArithmetic(int[,] array)
+int[,] MassNums(int row, int column, int from, int to)
 {
-    int rows = array.GetLength(0);
-    int columns = array.GetLength(1);
-    double[] avg = new double[columns];
+    int[,] arr = new int[row, column];
 
-    for (int j = 0; j < columns; j++)
-    {
-        int sum = 0;
-        for (int i = 0; i < rows; i++)
-        {
-            sum += array[i, j];
-        }
-        avg[j] = Math.Round((double)sum / rows, 1);
-    }
-    return avg;
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
 }
 
-void PrintArray(double[] array)
+void ArithmeticMean(int[,] arr)
 {
-    int length = array.Length;
-    for (int i = 0; i < length; i++)
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    double res;
+
+    for (int i = 0; i < column; i++)
     {
-        Console.Write(array[i]);
-        if (i < length - 1) Console.Write("; ");
-        else Console.WriteLine();
+        res = 0;
+        for (int j = 0; j < row; j++) res += arr[j, i];
+        Console.Write($"{Math.Round(res / row, 2)}; ");
     }
 }
 
-int[,] array = DoubleArray(3, 4, 1, 10);
-PrintDoubleArray(array);
-PrintArray(MeanArithmetic(array));
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column, 1, 6);
+Print(arr_1);
+
+ArithmeticMean(arr_1);
